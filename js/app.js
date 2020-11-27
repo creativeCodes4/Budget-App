53592183b1395b2d7c33755859b6009afdd7814b
IssaKevin <issa.kevin.code@gmail.com>
2020-11-27 10:55:48 -0800

    Added new Features

--- js/app.js
+++ js/app.js
@@ -12,9 +12,17 @@ const addIncomeButton = document.querySelector('#add-income-btn');
 
 const incomeInput = document.querySelector('#income-input');
 
+const expensesClass = document.querySelector('.expenses');
+
+const addExpenseButton = document.querySelector('#add-expense-btn');
+
+const expenseInput = document.querySelector('#expense-input');
+
+const expenseAmount = document.querySelector('#expense-amount');
+
 // Arrays to be used...
 
-let incomeArray = [100, 1222];
+let income;
 let expensesArray = [];
 
 // menuButtons[0].style.borderBottom = '3px solid rgb(126, 69, 218)';
@@ -29,6 +37,7 @@ menuButtons.forEach((button, index) => {
     cards.forEach((card) => {
       if (card.classList.contains(btn)) {
         card.style.display = 'block';
+        card.style.animation = 'modalanim 1s ease forwards';
         button.backgroundColor = 'rgb(126, 69, 218)';
       } else {
         card.style.display = 'none';
@@ -42,37 +51,38 @@ menuButtons.forEach((button, index) => {
 
 addIncomeButton.addEventListener('click', () => {
   const newIncomeValue = incomeInput.value;
-  incomeArray.push(newIncomeValue);
+  income = newIncomeValue;
+  incomeAmount.textContent = income;
   incomeInput.value = '';
-  console.log(incomeArray);
 });
 
-// Showing Incomes...
-let total = 0;
 
-// Adding Income Function...
+// Listening on Adding Expense Button...
 
-function addNewIncome(incomeValue) {
-  incomeArray.push(incomeValue.parseInt());
-  incomeInput.value = '';
-  showIncomes();
-}
 
-// Showin Income Lists...
+addExpenseButton.addEventListener('click', () => {
+  const newExpenseValue = expenseInput.value;
+  expense = newExpenseValue;
+  expensesArray.push(expense);
+  expenseInput.value = '';
+  showExpenses();
+});
 
-function showIncomes() {
-  const incomeUl = document.createElement('ul');
-  const newTotal = document.createTextNode(total);
-  incomeArray.forEach((item) => {
-    const incomeLi = document.createElement('li');
-    total = total + item;
-    let income = document.createTextNode(item);
-    incomeLi.appendChild(income);
-    incomeUl.appendChild(incomeLi);
+function showExpenses(){
+  let total = 0;
+
+  // Creating new UL and Li elements...
+
+  const expenseUl = document.createElement('ul');
+  expensesClass.innerHTML = '';
+  // expenseUl.innerHTML = '';
+  expensesArray.forEach( (item) => {
+    expenseUl.innerHTML += `
+      <li>${item}</li>
+    `;    
+    total += parseFloat(item);
+    expensesClass.appendChild(expenseUl);
   });
+  expenseAmount.textContent = total;
 
-  incomes.appendChild(incomeUl);
-
-  incomeAmount.textContent = total;
 }
-
